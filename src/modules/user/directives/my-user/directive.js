4c4fcea59015a85139bfd79412db8a1e61c76c02
Kiwouz <espritkiwi@gmail.com>
2016-10-14 16:18:22 +0200

    Feat : update demo page user

--- src/modules/user/directives/my-user/directive.js
+++ src/modules/user/directives/my-user/directive.js
@@ -5,9 +5,10 @@ angular.module('eklabs.angularStarterPack.user')
         return {
             templateUrl : 'eklabs.angularStarterPack/modules/user/directives/my-user/view.html',
             scope : {
-
+                user : '=?',
+                callback : '=?'
             },link : function(scope){
-                alert('youhouuuuu');
+                
             }
         }
     })
